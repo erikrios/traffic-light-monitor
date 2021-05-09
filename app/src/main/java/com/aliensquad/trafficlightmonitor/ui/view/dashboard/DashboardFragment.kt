684656f876b1f64df24dd3b4aefc8037d541270c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-09 11:14:20 +0700

    Create DashboardFragment.kt

---
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/dashboard/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.aliensquad.trafficlightmonitor.ui.view.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aliensquad.trafficlightmonitor.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
