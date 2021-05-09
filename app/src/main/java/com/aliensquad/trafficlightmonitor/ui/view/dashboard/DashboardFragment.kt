3e260da20c5b893fa246bae701ad326e927c417b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-09 20:20:52 +0700

    Add a method to handle recycler view

--- app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/dashboard/DashboardFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.aliensquad.trafficlightmonitor.databinding.FragmentDashboardBinding
+import com.aliensquad.trafficlightmonitor.ui.adapter.TrafficLightAdapter
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding
+    private val adapter = TrafficLightAdapter {
+        Toast.makeText(context, it.name, Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +23,15 @@ class DashboardFragment : Fragment() {
     ): View? {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleRecyclerView()
+    }
+
+    private fun handleRecyclerView() {
+        binding?.rvTrafficLights?.adapter = adapter
     }
 
     override fun onDestroyView() {
