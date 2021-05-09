9659658ee1b9b5e2cac4d383eb5da6ae060411cc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-09 11:57:40 +0700

    Create Result.kt

--- app/src/main/java/com/aliensquad/trafficlightmonitor/data/model/Result.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/data/model/Result.kt
@@ -1,7 +1,12 @@
 package com.aliensquad.trafficlightmonitor.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Result<T>(
+    @SerializedName("status")
     val status: String,
+    @SerializedName("message")
     val message: String? = null,
+    @SerializedName("data")
     val data: T? = null
 )
