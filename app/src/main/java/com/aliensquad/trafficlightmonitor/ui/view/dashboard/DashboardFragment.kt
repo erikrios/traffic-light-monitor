3653b57f5285d46d82be17f4356e5497204463e1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-09 20:40:38 +0700

    Add a method to handle spinner

--- app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/dashboard/DashboardFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.aliensquad.trafficlightmonitor.databinding.FragmentDashboardBinding
 import com.aliensquad.trafficlightmonitor.ui.adapter.TrafficLightAdapter
+import com.aliensquad.trafficlightmonitor.utils.DummyData.generateTrafficLights
+import com.aliensquad.trafficlightmonitor.utils.RadiusConfiguration
+import com.aliensquad.trafficlightmonitor.utils.RadiusConfiguration.generateRadiusValues
 
 class DashboardFragment : Fragment() {
 
@@ -28,10 +33,44 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         handleRecyclerView()
+        handleSpinner()
     }
 
     private fun handleRecyclerView() {
         binding?.rvTrafficLights?.adapter = adapter
+    }
+
+    private fun handleSpinner() {
+        val radiusValues = generateRadiusValues()
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_dropdown_item,
+            radiusValues
+        )
+        binding?.spinnerRadius?.apply {
+            this.adapter = adapter
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val radius = RadiusConfiguration.getRadiusFromIndex(position)
+                    Toast.makeText(context, radius.distance.toString(), Toast.LENGTH_SHORT).show()
+                    this@DashboardFragment.adapter.setTrafficLights(generateTrafficLights().shuffled())
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Toast.makeText(
+                        context,
+                        RadiusConfiguration.Radius.KM_3.distance.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    this@DashboardFragment.adapter.setTrafficLights(generateTrafficLights())
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
