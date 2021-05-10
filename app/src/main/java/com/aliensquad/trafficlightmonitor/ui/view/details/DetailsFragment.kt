d468004d02d1427e5ba1ec5dd0f1f04e94f58ce0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-10 13:34:28 +0700

    Add a method to handle toolbar

--- app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/details/DetailsFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/details/DetailsFragment.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.aliensquad.trafficlightmonitor.R
 import com.aliensquad.trafficlightmonitor.databinding.FragmentDetailsBinding
+import com.aliensquad.trafficlightmonitor.utils.DummyData.getTrafficLight
 
 class DetailsFragment : Fragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding
+    private val args: DetailsFragmentArgs by navArgs()
+    private val trafficLight = getTrafficLight(args.trafficLight.id)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +27,21 @@ class DetailsFragment : Fragment() {
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun handleToolbar(title: String) {
+        binding?.toolbar?.apply {
+            this.title = title
+            navigationIcon =
+                ContextCompat.getDrawable(context, R.drawable.ic_baseline_arrow_back_24)
+            setNavigationOnClickListener { findNavController().popBackStack() }
+        }
     }
 }
