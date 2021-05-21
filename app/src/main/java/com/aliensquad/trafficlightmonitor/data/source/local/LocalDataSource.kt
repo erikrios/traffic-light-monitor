b3b868d2ecabbeebe00e122516aa078b54806511
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-21 15:52:49 +0700

    Create LocalDataSource.kt

--- app/src/main/java/com/aliensquad/trafficlightmonitor/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/data/source/local/LocalDataSource.kt
@@ -2,19 +2,35 @@ package com.aliensquad.trafficlightmonitor.data.source.local
 
 import com.aliensquad.trafficlightmonitor.data.model.TrafficLight
 import com.aliensquad.trafficlightmonitor.data.source.DataSource
+import com.aliensquad.trafficlightmonitor.utils.RadiusConfiguration
 import com.aliensquad.trafficlightmonitor.utils.Resource
 
 class LocalDataSource : DataSource {
 
-    private val trafficLightCaches = mutableSetOf<TrafficLight>()
+    private val trafficLightCaches = mutableMapOf<RadiusConfiguration.Radius, List<TrafficLight>>()
 
-    override suspend fun getTrafficLights(): Resource<List<TrafficLight>> =
-        Resource.success(trafficLightCaches.toList())
-
-    override suspend fun getTrafficLightDetails(id: Long): Resource<TrafficLight> {
-        val trafficLight = trafficLightCaches.find { id == it.id }
-        return Resource.success(trafficLight)
+    override suspend fun getTrafficLights(radius: RadiusConfiguration.Radius): Resource<List<TrafficLight>> {
+        val trafficLights = trafficLightCaches[radius]
+        if (trafficLights.isNullOrEmpty()) {
+            return Resource.error("The data is null or empty.", null)
+        }
+        return Resource.success(trafficLights)
     }
 
-    fun addCaches(trafficLights: List<TrafficLight>) = trafficLightCaches.addAll(trafficLights)
+    override suspend fun getTrafficLightDetails(id: Long): Resource<TrafficLight> {
+        val trafficLights = mutableListOf<TrafficLight>()
+        val listOfTrafficLights = trafficLightCaches.values.toList()
+        listOfTrafficLights.forEach {
+            it.forEach { trafficLight ->
+                trafficLights.add(trafficLight)
+            }
+        }
+
+        val trafficLight = trafficLights.find { it.id == id }
+        return trafficLight?.let { Resource.success(trafficLight) }
+            ?: Resource.error("The data not found", null)
+    }
+
+    fun addCaches(radius: RadiusConfiguration.Radius, trafficLights: List<TrafficLight>) =
+        trafficLightCaches.put(radius, trafficLights)
 }
