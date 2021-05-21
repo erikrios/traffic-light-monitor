b4be6ebdc5e328a034837571681f434e1e049753
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-21 15:32:19 +0700

    Create RemoteDataSource.kt

---
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,21 @@
+package com.aliensquad.trafficlightmonitor.data.source.remote
+
+import com.aliensquad.trafficlightmonitor.data.model.TrafficLight
+import com.aliensquad.trafficlightmonitor.data.source.DataSource
+import com.aliensquad.trafficlightmonitor.utils.DummyData.generateTrafficLights
+import com.aliensquad.trafficlightmonitor.utils.DummyData.getTrafficLight
+import com.aliensquad.trafficlightmonitor.utils.RadiusConfiguration
+import com.aliensquad.trafficlightmonitor.utils.Resource
+
+class RemoteDataSource : DataSource {
+
+    override suspend fun getTrafficLights(radius: RadiusConfiguration.Radius): Resource<List<TrafficLight>> {
+        val trafficLights = generateTrafficLights()
+        return Resource.success(trafficLights)
+    }
+
+    override suspend fun getTrafficLightDetails(id: Long): Resource<TrafficLight> {
+        val trafficLight = getTrafficLight(id)
+        return Resource.success(trafficLight)
+    }
+}
