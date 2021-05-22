5c352f9ca32624a44caea6de5fce297c7ef0fe80
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-22 21:02:56 +0700

    Create PermissionUtils.kt

---
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/utils/PermissionUtils.kt
@@ -0,0 +1,52 @@
+package com.aliensquad.trafficlightmonitor.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.aliensquad.trafficlightmonitor.R
+
+object PermissionUtils {
+
+    fun requestAccessFineLocationPermission(activity: AppCompatActivity, requestId: Int) {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            requestId
+        )
+    }
+
+    fun isAccessFineLocationGranted(context: Context): Boolean {
+        return ContextCompat
+            .checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun isLocationEnabled(context: Context): Boolean {
+        val locationManager: LocationManager =
+            context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    fun showGPSNotEnableDialog(context: Context) {
+        AlertDialog.Builder(context)
+            .setTitle(context.getString(R.string.enable_gps))
+            .setMessage(context.getString(R.string.enable_gps_required_message))
+            .setCancelable(false)
+            .setPositiveButton(context.getString(R.string.enable_now)) { _, _ ->
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                context.startActivity(intent)
+            }
+            .show()
+    }
+}
