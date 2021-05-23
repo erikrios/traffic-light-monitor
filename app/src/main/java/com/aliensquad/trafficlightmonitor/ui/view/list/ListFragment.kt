169442417f906a720df7a1d6b413dd37b0517da4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-23 17:01:45 +0700

    Integrated the view model

--- app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/list/ListFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/list/ListFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.aliensquad.trafficlightmonitor.data.model.TrafficLight
 import com.aliensquad.trafficlightmonitor.databinding.FragmentListBinding
+import com.aliensquad.trafficlightmonitor.ui.adapter.TrafficLightAdapter
+import com.aliensquad.trafficlightmonitor.ui.view.dashboard.DashboardFragmentDirections
+import com.aliensquad.trafficlightmonitor.ui.viewmodel.DashboardViewModel
+import com.aliensquad.trafficlightmonitor.utils.Resource
+import com.aliensquad.trafficlightmonitor.utils.Status
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 
 class ListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding
+    private val dashboardViewModel: DashboardViewModel by sharedViewModel()
+    private val adapter = TrafficLightAdapter { navigateToDetailsFragment(it) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +30,48 @@ class ListFragment : Fragment() {
     ): View? {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleRecyclerView()
+        dashboardViewModel.trafficLightsState.observe(
+            viewLifecycleOwner,
+            this@ListFragment::handleState
+        )
+    }
+
+    private fun handleState(resource: Resource<List<TrafficLight>>) {
+        when (resource.status) {
+            Status.LOADING -> handleLoadingState()
+            Status.ERROR -> handleErrorState(resource.message.toString())
+            Status.SUCCESS -> handleSuccessState(resource.data)
+        }
+    }
+
+    private fun handleLoadingState() {
+        binding?.progressBar?.visibility = View.VISIBLE
+    }
+
+    private fun handleErrorState(message: String) {
+        binding?.progressBar?.visibility = View.GONE
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleSuccessState(trafficLights: List<TrafficLight>?) {
+        binding?.progressBar?.visibility = View.GONE
+        trafficLights?.let {
+            adapter.setTrafficLights(it)
+        }
+    }
+
+    private fun handleRecyclerView() {
+        binding?.rvTrafficLights?.adapter = adapter
+    }
+
+    private fun navigateToDetailsFragment(trafficLight: TrafficLight) {
+        val action =
+            DashboardFragmentDirections.actionDashboardFragmentToDetailsFragment(trafficLight)
+        findNavController().navigate(action)
     }
 }
