2500c8f48b22061d6c476565bb8cb6fac3869a11
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-24 13:02:58 +0700

    Integrated the view model

--- app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/maps/MapsFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/ui/view/maps/MapsFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.aliensquad.trafficlightmonitor.databinding.FragmentMapsBinding
+import com.aliensquad.trafficlightmonitor.ui.viewmodel.DashboardViewModel
+import org.koin.android.viewmodel.ext.android.getViewModel
 
 class MapsFragment : Fragment() {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding
+    private val dashboardViewModel by lazy {
+        requireParentFragment().getViewModel<DashboardViewModel>()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
