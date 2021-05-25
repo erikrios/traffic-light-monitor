705128dcf4cd284cf2ea52bdd5e0908b6f687b18
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-25 10:19:29 +0700

    Integrated the view binding

--- app/src/main/java/com/aliensquad/trafficlightmonitor/route/RouteFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/route/RouteFragment.kt
@@ -1,19 +1,27 @@
 package com.aliensquad.trafficlightmonitor.route
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.aliensquad.trafficlightmonitor.R
+import androidx.fragment.app.Fragment
+import com.aliensquad.trafficlightmonitor.databinding.FragmentRouteBinding
 
 class RouteFragment : Fragment() {
+
+    private var _binding: FragmentRouteBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_route, container, false)
+        _binding = FragmentRouteBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
