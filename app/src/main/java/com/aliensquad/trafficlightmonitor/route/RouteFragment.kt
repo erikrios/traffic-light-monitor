a771412a4ad431171287ee5ad9ed9547bb061dc7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-25 10:43:51 +0700

    Get the arguments

--- app/src/main/java/com/aliensquad/trafficlightmonitor/route/RouteFragment.kt
+++ app/src/main/java/com/aliensquad/trafficlightmonitor/route/RouteFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.aliensquad.trafficlightmonitor.databinding.FragmentRouteBinding
 
 class RouteFragment : Fragment() {
 
     private var _binding: FragmentRouteBinding? = null
     private val binding get() = _binding
+    private val args: RouteFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +20,10 @@ class RouteFragment : Fragment() {
     ): View? {
         _binding = FragmentRouteBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
